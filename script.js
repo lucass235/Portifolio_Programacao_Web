5c56f869c3012d5014236259e0694ee960bdeabe
lucas <lucass235@gmail.com>
2022-04-18 17:22:28 -0300

    .

--- script.js
+++ script.js
@@ -10,27 +10,14 @@ function submit(event) {
   } else {
     alert(
       name +
-        " sua mensagem foi enviada com suceeso" +
-        "\n" +
-        "Seu email: " +
-        email +
-        "\n" +
-        "Sua mensagem: " +
-        msg
+      " sua mensagem foi enviada com suceeso" +
+      "\n" +
+      "Seu email: " +
+      email +
+      "\n" +
+      "Sua mensagem: " +
+      msg
     );
     form.reset();
   }
 }
-
-// function sendEmail(name, email, msg) {
-//   email
-//     .send({
-//       Host: "smtp.gmail.com",
-//       Username: "lucas.2020109680@unicap.br",
-//       Password: "iejjdqixuiidmxwh",
-//       To: "lucas.2020109680@unicap.br",
-//       From: "lucas.2020109680@unicap.br",
-//       Subject: `${name} - Contato pelo site`,
-//       Body: `Nome: ${name} <br/> Email:  ${email} <br/> Mensagem: ${msg}`,
-//     })
-//     .then((message) => alert("Mensagem enviada com sucesso!"));
